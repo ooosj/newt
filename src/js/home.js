f877fb9c28713cc13d065b913d1a44334102154c
ooosj <tmdwn8775@gmail.com>
2024-06-07 23:41:15 +0900

    detail 상단바 및 css 수정

--- src/js/home.js
+++
@@ -1,9 +0,0 @@
-class Home {
-    template() {
-      return `
-      <div id = "a">Home Page</div>
-      <button class="moveToAboutPageBtn">Go to About Page</button>
-      `;
-    }
-  }
-  export default new Home();
