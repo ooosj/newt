7bb7946ddc2e9ff5cdd82438aecaafbbe085eb44
ooosj <tmdwn8775@gmail.com>
2024-06-07 22:14:17 +0900

    메인페이지 로그아웃 구현

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const userName = window.sessionStorage.getItem('username');
     if (userName !== null) {
         // If logged in, show username
-        loginArea.innerHTML = `<span>${userName}</span>`;
+        loginArea.innerHTML = `
+            <span style="margin-left:20px";>${userName}님</span>
+            <button id="logoutButton" style="margin-left:20px";> 로그아웃 </button>
+        `;
         console.log(1);
+
+        const logoutButton = document.getElementById('logoutButton');
+        logoutButton.addEventListener('click', function() {
+            alert('로그아웃 되었습니다.');
+            window.sessionStorage.removeItem('username'); // Remove username from session storage
+            location.reload(); // Reload the page
+        });
     } else {
         // If not logged in, show login button
         loginArea.innerHTML = `<button onClick="location.href='./login.html'">로그인</button>`;
@@ -15,7 +25,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    
     const container = document.getElementById('container');
     let currentIndex = 0; // 현재 뉴스 인덱스
     const imageUrl = '../images/ms.jpg'; // 테스트 이미지 URL
@@ -63,4 +72,6 @@ function createNewsPlaceholders(count) {
 }
 
 });
+
+
 
