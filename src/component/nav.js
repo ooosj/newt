8fc3fa235a38f3d52e1290e6e77a2a609b5d4ae2
ooosj <tmdwn8775@gmail.com>
2024-06-12 04:10:10 +0900

    .....

--- src/component/nav.js
+++ src/component/nav.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const loginArea = document.getElementById('login-area');
     const userName = window.sessionStorage.getItem('username');
     if (userName !== null) {
-        // If logged in, show username
+        // 로그인 되어있을 때, 유저 이름을 보여줌
         loginArea.innerHTML = `
             <span style="margin-left:20px; font-size : 18px";>${userName}님</span>
             <button id="logoutButton" style="margin-left:20px";> 로그아웃 </button>
@@ -12,12 +12,14 @@ document.addEventListener('DOMContentLoaded', function() {
         const logoutButton = document.getElementById('logoutButton');
         logoutButton.addEventListener('click', function() {
             alert('로그아웃 되었습니다.');
-            window.sessionStorage.removeItem('username'); // Remove username from session storage
-            location.reload(); // Reload the page
+            window.sessionStorage.removeItem('username'); // 세션 스토리지에서 유저 이름 삭제
+            location.reload(); 
         });
     } else {
-        // If not logged in, show login button
+        // 로그인 되어있지 않을 때, 로그인 버튼을 보여줌
         loginArea.innerHTML = `<button onClick="location.href='./login.html'">로그인</button>`;
         console.log(2);
     }
 });
+
+
